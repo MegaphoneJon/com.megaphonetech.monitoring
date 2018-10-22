4145f29d591c64c6be7c1c401c013ef6e655ffb9
Jon Goldberg <jon@megaphonetech.com>
2018-10-22 03:46:42 -0400

    initial commit

---
+++ monitoring.php
@@ -0,0 +1,180 @@
+<?php
+
+require_once 'monitoring.civix.php';
+use CRM_Monitoring_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_permission().
+ */
+function monitoring_civicrm_permission(&$permissions) {
+  $permissions += ['remote monitoring' =>
+                    [ts('CiviCRM Remote Monitoring', ['domain' => 'com.megaphonetech.monitoring']),
+                     ts('Grants the necessary API permissions for a monitoring user without Administer CiviCRM', ['domain' => 'com.megaphonetech.monitoring']),
+                    ],
+                  ];
+}
+/**
+ * Implements hook_civicrm_alterAPIPermissions().
+ */
+function monitoring_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions) {
+  $permissions['system']['check'] = ['remote monitoring'];
+}
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function monitoring_civicrm_config(&$config) {
+  _monitoring_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function monitoring_civicrm_xmlMenu(&$files) {
+  _monitoring_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function monitoring_civicrm_install() {
+  _monitoring_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function monitoring_civicrm_postInstall() {
+  _monitoring_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function monitoring_civicrm_uninstall() {
+  _monitoring_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function monitoring_civicrm_enable() {
+  _monitoring_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function monitoring_civicrm_disable() {
+  _monitoring_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function monitoring_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _monitoring_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function monitoring_civicrm_managed(&$entities) {
+  _monitoring_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function monitoring_civicrm_caseTypes(&$caseTypes) {
+  _monitoring_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function monitoring_civicrm_angularModules(&$angularModules) {
+  _monitoring_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function monitoring_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _monitoring_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
+ */
+function monitoring_civicrm_entityTypes(&$entityTypes) {
+  _monitoring_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function monitoring_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function monitoring_civicrm_navigationMenu(&$menu) {
+  _monitoring_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _monitoring_civix_navigationMenu($menu);
+} // */
