ba5b9e8d93b1db97eae270a38323c55e9fc692aa
Jon Goldberg <jon@megaphonetech.com>
2025-03-25 19:10:40 -0400

    one more - change message name

--- monitoring.php
+++ monitoring.php
@@ -21,7 +21,7 @@ function monitoring_civicrm_check(&$messages) {
 function monitoring_checkXdebug(&$messages) {
   if (function_exists('xdebug_is_debugger_active') && xdebug_is_debugger_active()) {
     $messages[] = new CRM_Utils_Check_Message(
-      __FUNCTION__,
+      'checkXdebug',
       ts('XDebug must be turned off on this site.'),
       ts('XDebug is enabled'),
       \Psr\Log\LogLevel::WARNING,
